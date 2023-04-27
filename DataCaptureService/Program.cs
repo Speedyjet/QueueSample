9dd34f9aff9586337d7e268e8983db02654b4ef9
Oleh Brusentsov <Oleh_Brusentsov@epam.com>
2023-04-27 22:17:38 +0200

    Add project files.

---
+++ DataCaptureService/Program.cs
@@ -0,0 +1,9 @@
+﻿//Configure service
+
+//Create queue
+
+//Capturing services periodically send current status to the Main processing service: 
+//Current service status (waiting for files/processing file) 
+//Current status: 
+//Max size of the message 
+//Additional info (if necessary) 
---
+++ MainProcessingService/Program.cs
@@ -0,0 +1,8 @@
+﻿//TODO Set Configuration
+
+//Get Statuses
+
+//Update status(force update before waiting for the status from the services)
+//Change settings: 
+//Max size of the message 
+//Additional settings (if necessary) 
